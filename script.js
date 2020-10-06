c41ea7496dd9b61f40ed40fb5404527d4515421a
chris kim <chriskim0926@gmail.com>
2020-10-06 18:40:28 -0400

    added display date function

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+
+    // Show Current Date 
+    function getDateContext() {
+    var month = new Array();
+        month[0] = "January";
+        month[1] = "February";
+        month[2] = "March";
+        month[3] = "April";
+        month[4] = "May";
+        month[5] = "June";
+        month[6] = "July";
+        month[7] = "August";
+        month[8] = "September";
+        month[9] = "October";
+        month[10] = "November";
+        month[11] = "December";
+    
+    var weekday = new Array(7);
+        weekday[0] = "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
+        
+        var d = new Date();
+        var n = month[d.getMonth()];
+        var day = weekday[d.getDay()];
+        var numDay = d.getDate();
+        console.log(day + ", " +  n +" "+ numDay)
+        document.getElementById("currentDay").innerHTML = day + ", " +  n +" "+ numDay;
+      }
+    
+      getDateContext()
+})
